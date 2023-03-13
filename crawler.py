5daba9ce9974ed7b5e2d9c8e1606e7997e3ba84b
Trong Nguyen <trtnguyen1@gmail.com>
2023-03-14 00:45:45 +1100

    scrapes google restaurants

--- crawler.py
+++ crawler.py
@@ -1,5 +1,3 @@
-# TIP: use 'py' to run file in terminal
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -14,11 +12,6 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get("https://www.google.com/search?q=restaurants+in+sydney&oq=restaurants+in+sydney&aqs=chrome.0.69i59j0i10i433i457i512j0i402j0i10i402i433i512j0i10i512j69i60l3.4986j0j7&sourceid=chrome&ie=UTF-8")
 driver.maximize_window()
-
-#target = '//span[@class="OSrXXb"]'
-#links = driver.find_elements("xpath", target)
-#for link in links:
-#    print(link.get_attribute("innerHTML"))
 
 x_more_places = '//span[@class="Z4Cazf OSrXXb"]'
 links = driver.find_elements("xpath", x_more_places)
