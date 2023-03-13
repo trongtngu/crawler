70d439ca1ba69f26c8e0062e225526b06f3a6cdb
Trong Nguyen <trtnguyen1@gmail.com>
2023-03-14 00:41:49 +1100

    scrapes google restaurants

--- crawler.py
+++ crawler.py
@@ -1,19 +1,33 @@
-
 # TIP: use 'py' to run file in terminal
 
 from selenium import webdriver
-import time
-
 from selenium.webdriver.chrome.service import Service
 
-def launchBrowser ():
-    driver_service = Service(executable_path="C:\Program Files (x86)\chromedriver.exe")
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-    PATH = "C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Chrome(service=driver_service)
+import time
 
-    driver.get("https://google.com")
-    while(True):
-        pass
+options = Options()
+options.add_experimental_option("detach", True)
 
-launchBrowser()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://www.google.com/search?q=restaurants+in+sydney&oq=restaurants+in+sydney&aqs=chrome.0.69i59j0i10i433i457i512j0i402j0i10i402i433i512j0i10i512j69i60l3.4986j0j7&sourceid=chrome&ie=UTF-8")
+driver.maximize_window()
+
+#target = '//span[@class="OSrXXb"]'
+#links = driver.find_elements("xpath", target)
+#for link in links:
+#    print(link.get_attribute("innerHTML"))
+
+x_more_places = '//span[@class="Z4Cazf OSrXXb"]'
+links = driver.find_elements("xpath", x_more_places)
+links[0].click()
+
+x_restaurants = '//span[@class="OSrXXb"]'
+restaurants = driver.find_elements("xpath", x_restaurants)
+for r in restaurants:
+    print(r.get_attribute("innerHTML"))
+
+driver.close()
+driver.quit()
